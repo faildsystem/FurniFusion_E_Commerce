476efb8e697f0b9842388a703d194975a0f385df
abdeaziz ahmed <abdelazizahemd1811@gmail.com>
2024-10-10 00:25:36 +0300

    upload and update user profile image upload and update product images

--- FurniFusion(E-Commerce)/Controllers/ProductManager/ProductController.cs
+++ FurniFusion(E-Commerce)/Controllers/ProductManager/ProductController.cs
@@ -15,6 +15,9 @@ namespace FurniFusion.Controllers
     {
         private readonly IProductService _productService;
 
+
+
+
         public ProductController(IProductService productService)
         {
             _productService = productService;
